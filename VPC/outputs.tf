d809c8edf2a1ab7e7ced8d1277d53706f0efea1a
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2025-08-04 14:37:53 +0530

    Create outputs.tf

---
+++ VPC/outputs.tf
@@ -0,0 +1,23 @@
+# =============================================================================
+# NETWORK INFORMATION
+# =============================================================================
+
+output "vpc_id" {
+  description = "ID of the VPC where the cluster is deployed"
+  value       = module.vpc.vpc_id
+}
+
+output "vpc_cidr_block" {
+  description = "CIDR block of the VPC"
+  value       = module.vpc.vpc_cidr_block
+}
+
+output "private_subnets" {
+  description = "List of IDs of private subnets"
+  value       = module.vpc.private_subnets
+}
+
+output "public_subnets" {
+  description = "List of IDs of public subnets"
+  value       = module.vpc.public_subnets
+}
