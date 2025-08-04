79e8ac042a38a1c2de7421f49f5ca8164f24498f
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2025-08-04 15:09:36 +0530

    Create outputs.tf

---
+++ EKS/outputs.tf
@@ -0,0 +1,62 @@
+# =============================================================================
+# NETWORK INFORMATION
+# =============================================================================
+
+output "vpc_id" {
+  description = "ID of the VPC where the cluster is deployed"
+  value       = module.vpc.vpc_id
+}
+
+output "vpc_cidr_block" {
+  description = "CIDR block of the VPC"
+  value       = module.vpc.vpc_cidr_block
+}
+
+output "private_subnets" {
+  description = "List of IDs of private subnets"
+  value       = module.vpc.private_subnets
+}
+
+output "public_subnets" {
+  description = "List of IDs of public subnets"
+  value       = module.vpc.public_subnets
+}
+
+# =============================================================================
+# CLUSTER INFORMATION
+# =============================================================================
+
+output "cluster_name" {
+  description = "Name of the EKS cluster (with unique suffix)"
+  value       = module.eks.cluster_name
+}
+
+output "cluster_name_base" {
+  description = "Base cluster name without suffix"
+  value       = var.cluster_name
+}
+
+output "cluster_name_suffix" {
+  description = "Random suffix added to cluster name"
+  value       = random_string.suffix.result
+}
+
+output "cluster_endpoint" {
+  description = "Endpoint for EKS control plane"
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_version" {
+  description = "The Kubernetes version for the EKS cluster"
+  value       = module.eks.cluster_version
+}
+
+output "cluster_security_group_id" {
+  description = "Security group ID attached to the EKS cluster"
+  value       = module.eks.cluster_security_group_id
+}
+
+output "cluster_oidc_issuer_url" {
+  description = "The URL on the EKS cluster for the OpenID Connect identity provider"
+  value       = module.eks.cluster_oidc_issuer_url
+}
