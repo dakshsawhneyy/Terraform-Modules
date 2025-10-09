cd07be10147abad6a4b27f0bd1baa62d28449036
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2025-10-09 17:01:40 +0530

    Update main.sh

--- Remote_State_File/main.sh
+++ Remote_State_File/main.sh
@@ -4,12 +4,12 @@ resource "aws_s3_bucket" "my_bucket" {
 
 # Locking mechanism using DynamoDB
 resource "aws_dynamodb_table" "terraform_lock" {
-    name = "terraform-lock"
-    billing_mode = "PAY_PER_REQUEST"
-    hash_key = "LockID"
-    
-    attribute {
-        name = "LockID"
-        type = "S"
-    }
+    name = "terraform-lock"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key = "LockID"
+
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
 }
